ed7d7c609f09549c3ddb23f025954d049fa96c79
khalid-00001 <khalid.249000@gmail.com>
2024-12-06 19:21:36 +0200

    Add files via upload

---
+++ PatternNuberPyramidPrevRev.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class PatternNuberPyramidPrevRev {
+
+    public static void main(String[] args) {
+        int r = 5;
+        System.out.print(r / 0);
+
+    }
+}
